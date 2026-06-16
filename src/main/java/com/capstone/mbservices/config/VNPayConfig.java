6487051430f60b983f455b40211095d897b7b085
Minh Tuan <tuan.pham.set18@eiu.edu.vn>
2026-06-17 06:59:16 +0700

    nothing

--- src/main/java/com/capstone/mbservices/config/VNPayConfig.java
+++ src/main/java/com/capstone/mbservices/config/VNPayConfig.java
@@ -120,11 +120,15 @@ public class VNPayConfig {
         String ipAdress;
         try {
             ipAdress = request.getHeader("X-FORWARDED-FOR");
-            if (ipAdress == null) {
+            if (ipAdress == null || ipAdress.isEmpty()) {
                 ipAdress = request.getRemoteAddr();
+            } else {
+                if (ipAdress.contains(",")) {
+                    ipAdress = ipAdress.split(",")[0].trim();
+                }
             }
         } catch (Exception e) {
-            ipAdress = "Invalid IP:" + e.getMessage();
+            ipAdress = "127.0.0.1";
         }
         return ipAdress;
     }
